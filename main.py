4c9cf345ad07dab0b4e9bdce0883d38b75eeeb8f
9takanotsume9 <9takanotsume9@gmail.com>
2019-05-01 14:36:05 +0900

    create base files

---
+++ main.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from esim.app import app
+
+
+# if __name__ == "__main__":
+    # config_path = Path(__file__).parent.resolve() / "config.cfg"
+    # app.config.from_pyfile(config_path)
+    # app.run()
